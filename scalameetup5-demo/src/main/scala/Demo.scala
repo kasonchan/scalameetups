a7d916547cbceee7e90a9cdc73199942d72dd889
kasonchan <kasonl.chan@gmail.com>
2017-08-17 08:18:51 +0100

    Add more topics Update scalastyle Add supervision strategies and routing

--- scalameetup5-demo/src/main/scala/Demo.scala
+++ scalameetup5-demo/src/main/scala/Demo.scala
@@ -1,9 +1,9 @@
 import actors.TimedWorker.FirstTick
-import actors.{DefaultConstructorWorker, TimedWorker, Worker}
+import actors.{DefaultConstructorWorker, Supervisor, TimedWorker, Worker}
 import akka.actor.{ActorRef, ActorSystem, Inbox, Props}
 import akka.pattern.{ask, pipe}
 import akka.util.Timeout
-import messages.{Hit, Fine, Ping}
+import messages.{Fine, Hit, Ping}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -27,21 +27,22 @@ object Demo {
 
     val you: ActorRef = system.actorOf(Props[Worker], "you")
 
-    me.tell(Ping, you)
+//    me.tell(Ping, you)
+//
+//    inbox.send(me, Ping)
+//    val reply = inbox.receive(5.seconds)
+//    print(s"$reply\n")
+//
+//    val result1: Future[Any] = ask(me, Ping)
+//    result1.onComplete(println(_))
+//
+//    val result2: Future[Any] = me ? Ping
+//    result2.onComplete(println(_))
+//    result2.pipeTo(you)
+//    pipe(result2) to you
 
-    inbox.send(me, Ping)
-    val reply = inbox.receive(5.seconds)
-    print(s"$reply\n")
-
-    val result1: Future[Any] = ask(me, Ping)
-    result1.onComplete(println(_))
-
-    val result2: Future[Any] = me ? Ping
-    result2.onComplete(println(_))
-    result2.pipeTo(you)
-    pipe(result2) to you
-
-    val dcw1: ActorRef = system.actorOf(DefaultConstructorWorker.props((1, false, "dc1")), "dcw1")
+    val dcw1: ActorRef =
+      system.actorOf(DefaultConstructorWorker.props((1, false, "dc1")), "dcw1")
 
     inbox.send(dcw1, "internalInteger")
     val dcw1Integer = inbox.receive(10.seconds)
@@ -62,6 +63,10 @@ object Demo {
     val timer: ActorRef = system.actorOf(Props[TimedWorker], "timer")
 
     timer ! FirstTick
+
+    val sp: ActorRef = system.actorOf(Props[Supervisor], "sp")
+
+    for (i <- 1 to 10) sp ! i
 
   }
 
