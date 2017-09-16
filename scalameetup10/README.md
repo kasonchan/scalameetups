66cd9558fae858cdcc18e43e97b5bed096da73c4
kasonchan <kasonl.chan@gmail.com>
2017-09-16 12:29:17 +0100

    Update scalameetup10 - Refactor Server.scala - Update notes on README.md

--- scalameetup10/README.md
+++ scalameetup10/README.md
@@ -1,2 +1,134 @@
 # Build a REST API with Akka HTTP
 
+Building a REST API with Akka HTTP
+
+---
+This presentation slides are built with REPLesent!
+
+REPLesent is a neat little tool to build presentations
+using the Scala REPL.
+---
+Akka HTTP
+
+- http://akka.io/docs/
+- Modern, fast, asynchronous, streaming-first HTTP server and client.
+- Implements a full server- and client-side HTTP stack on top of
+  akka-actor and \yakka-stream\s.
+- General toolkit for providing and consuming HTTP-based services.
+---
+Akka HTTP
+
+- akka-http
+  - Higher-level functionality, like (un)marshalling, (de)compression as well as
+    a powerful DSL for defining HTTP-based APIs on the server-side, this is the
+    recommended way to write HTTP servers with Akka HTTP. Details can be found
+    in the section High-level Server-Side API
+- akka-http-core
+  - A complete, mostly low-level, server- and client-side implementation of HTTP
+    (incl. WebSockets) Details can be found in sections Low-Level Server-Side
+    API and Consuming HTTP-based Services (Client-Side) akka-http-testkit
+    A test harness and set of utilities for verifying server-side service
+    implementations
+- akka-http-spray-json
+  - Predefined glue-code for (de)serializing custom types from/to JSON with
+    spray-json Details can be found here: JSON Support
+- akka-http-xml
+  - Predefined glue-code for (de)serializing custom types from/to XML with
+    scala-xml Details can be found here: XML Support
+---
+Implicit Parameters
+--
+
+```
+val value = 10
+def addX(x: Int): Int = value + x
+addX(3)
+```
+--
+
+```
+implicit val implicitValue = 10
+def implicitAddX(implicit x: Int): Int = value + x
+implicitAddX
+```
+---
+Implicit Parameters
+
+```
+implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+def getEmployee(id: Int)(implicit e: ExecutionContext): Future[Employee] = ???
+def getRole(employee :Employee)(implicit e: ExecutionContext): Future[Role] = ???
+val bigEmployee: Future[EmployeeWithRole] =
+  getEmployee(100).flatMap { e =>
+    getRole(e).map { r =>
+      EmployeeWithRole(e.id, e.name,r)
+    }
+  }
+```
+--
+
+```
+val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+val bigEmployee: Future[EmployeeWithRole] =
+  getEmployee(100)(ec).flatMap { e =>
+    getRole(e)(ec).map { r =>
+      EmployeeWithRole(e.id, e.name,r)
+    }(ec)
+  }(ec)
+```
+--
+
+- Implicit parameters are useful for removing boiler plate parameter passing and
+  can make our code more readable. So if you find yourself passing the same
+  value several times in quick succession, they can help hide the duplication.
+---
+Configuration
+
+- Like other Akka modules, Akka HTTP is can be configured via Typesafe Config.
+- We can provide an application.conf file application-specific settings that is
+  differ from the default one.
+- http://doc.akka.io/docs/akka-http/current/scala/http/configuration.html
+
+---
+Akka HTTP
+
+- Today session we will focus on The high-level, routing API of Akka HTTP
+  provides a DSL to describe HTTP routes and how they should be handled.
+- Each route is composed of one or more level of \yDirective\ss that narrows
+  down to handling one specific type of request.
+---
+Demo
+---
+References
+
+- http://doc.akka.io/docs/akka-http/current/scala/http/index.html
+---
+| Thank you for coming!
+
+| Q&A/Comments?/Suggestions?
+
+---
+
+This presentation is built with [REPLesent](https://github.com/marconilanna/REPLesent).
+
+To run the slides, first create the follow alias:
+
+```
+alias REPLesent='scala -Dscala.color -language:_ -nowarn -i REPLesent.scala'
+```
+
+Open the REPL and enter the statements below to start the presentation:
+
+```
+$ REPLesent
+Welcome to Scala 2.12.3 (Java HotSpot(TM) 64-Bit Server VM, Java 1.8.0_65).
+Type in expressions for evaluation. Or try :help.
+
+scala> val replesent = REPLesent(intp=$intp)
+replesent: REPLesent = REPLesent(0,0,scalameetup7.txt,true,true,scala.tools.nsc.interpreter.ILoop$ILoopInterpreter@38d308e7)
+
+scala> import replesent._
+import replesent._
+
+scala> f
+```
--- scalameetup10/src/main/scala/Server.scala
+++ scalameetup10/src/main/scala/Server.scala
@@ -1,8 +1,6 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport._
-import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
@@ -13,7 +11,7 @@ import spray.json.RootJsonFormat
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
-import scala.xml.Elem
+import scala.util.{Success, Try}
 
 /**
   * @author kasonchan
@@ -21,6 +19,10 @@ import scala.xml.Elem
   */
 object Server {
   def main(args: Array[String]): Unit = {
+    implicit val system: ActorSystem = ActorSystem("system")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
     implicit def myRejectionHandler =
       RejectionHandler
         .newBuilder()
@@ -48,101 +50,52 @@ object Server {
         }
         .result()
 
-    implicit val system: ActorSystem = ActorSystem("system")
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
-    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+    case class JsonCharacter(name: String, job: String)
+    implicit val characterFormat: RootJsonFormat[JsonCharacter] = jsonFormat2(
+      JsonCharacter)
 
-    case class Character(name: String)
-    implicit val characterFormat: RootJsonFormat[Character] = jsonFormat1(
-      Character)
-
-    case class DO(data: String)
-    implicit val doFormat: RootJsonFormat[DO] = jsonFormat1(DO)
-
-    val unsecureRoutes =
+    val routes =
       path("hello") {
         get {
           complete("Akka HTTP")
         }
-      } ~ path("character") {
+      } ~ path("jsoncharacter") {
         post {
-          entity(as[Character]) { character =>
-            complete(character.name)
+          entity(as[JsonCharacter]) { character =>
+            complete(s"${character.name} is a ${character.job}.")
           }
         }
-      } ~ path("xml") {
+      } ~ path("xmlcharacter") {
         post {
-          entity(as[DO]) { xml =>
-            complete(xml.data)
+          entity(as[String]) { xmlString =>
+            Try {
+              val xml = scala.xml.XML.loadString(xmlString)
+              (xml \\ "name").text + " is a " + (xml \\ "job").text + "."
+            } match {
+              case Success(s) => complete(s)
+              case _          => complete(BadRequest, "Invalid xml format!")
+            }
           }
         }
       }
 
-    //    val secureRoutes =
-    //      path("hello") {
-    //        get {
-    //          complete("Akka HTTPS")
-    //        }
-    //      }
+    val (host, port) = ("localhost", 9000)
 
-    //    val password
-    //      : Array[Char] = "change me".toCharArray // do not store passwords in code, read them from somewhere safe!
-    //
-    //    val ks: KeyStore = KeyStore.getInstance("PKCS12")
-    //    val keystore: InputStream =
-    //      getClass.getClassLoader.getResourceAsStream("server.p12")
-    //
-    //    require(keystore != null, "Keystore required!")
-    //    ks.load(keystore, password)
-    //
-    //    val keyManagerFactory: KeyManagerFactory =
-    //      KeyManagerFactory.getInstance("SunX509")
-    //    keyManagerFactory.init(ks, password)
-    //
-    //    val tmf: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
-    //    tmf.init(ks)
-    //
-    //    val sslContext: SSLContext = SSLContext.getInstance("TLS")
-    //    sslContext.init(keyManagerFactory.getKeyManagers,
-    //                    tmf.getTrustManagers,
-    //                    new SecureRandom)
-    //    val https: HttpsConnectionContext = ConnectionContext.https(sslContext)
+    val bindingFuture =
+      Http().bindAndHandle(routes, host, port)
 
-    val (host, securedPort, unsecuredPort) = ("localhost", 80, 9000)
-
-    val unsecureBindingFuture =
-      Http().bindAndHandle(unsecureRoutes, host, unsecuredPort)
-
-    unsecureBindingFuture.failed.foreach { ex =>
-      println(ex, "Failed to bind to {}:{}!", host, unsecuredPort)
+    bindingFuture.failed.foreach { ex =>
+      System.err.println(s"Failed to bind to $host:$port!")
     }
 
-    println(
-      s"Unsecured server online at https://$host:$unsecuredPort/\n" +
-        s"Press RETURN to stop...")
-
-    //    Http().setDefaultServerHttpContext(https)
-    //    val secureBindingFuture = Http().bindAndHandle(secureRoutes,
-    //                                                   "localhost",
-    //                                                   securedPort,
-    //                                                   connectionContext = https)
-
-    //    println(
-    //      s"Secured server online at https://localhost:9090/\nPress RETURN to stop...")
-
-    //    secureBindingFuture.failed.foreach { ex =>
-    //      println(ex, "Failed to bind to {}:{}!", host, securedPort)
-    //    }
+    println(s"Server online at http://$host:$port/")
+    println("Press RETURN to stop...")
 
     StdIn.readLine() // let it run until user presses return
 
-    unsecureBindingFuture
+    bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
-
-    //    secureBindingFuture
-    //      .flatMap(_.unbind()) // trigger unbinding from the port
-    //      .onComplete(_ => system.terminate()) // and shutdown when done
   }
 
 }
