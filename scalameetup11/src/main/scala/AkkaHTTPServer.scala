1e0d535a7897921c6847aff88579d8bc4f66a82f
kasonchan <kasonl.chan@gmail.com>
2017-09-25 00:03:36 +0100

    Update scalameetup11 - Extract routes form AkkaHTTPServer to HarryPotterService - Add SwaggerService generate swagger docs

--- scalameetup11/src/main/scala/AkkaHTTPServer.scala
+++ scalameetup11/src/main/scala/AkkaHTTPServer.scala
@@ -1,12 +1,7 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server._
 import akka.stream.ActorMaterializer
-import spray.json.DefaultJsonProtocol._
-import spray.json.RootJsonFormat
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
@@ -15,8 +10,6 @@ import scala.io.StdIn
   * @author kasonchan
   * @since Sep-2017
   */
-case class Message(statusCode: Int, messages: Seq[String])
-
 object AkkaHTTPServer {
 
   implicit val system: ActorSystem = ActorSystem("system")
@@ -25,6 +18,8 @@ object AkkaHTTPServer {
 
   def main(args: Array[String]): Unit = {
     val (host, port) = ("localhost", 9000)
+
+    val routes = new HarryPotterService().routes ~ SwaggerService.routes
 
     val bindingFuture =
       Http().bindAndHandle(routes, host, port)
@@ -43,27 +38,5 @@ object AkkaHTTPServer {
       .flatMap(_.unbind())
       .onComplete(_ => system.terminate())
   }
-
-  implicit val messageFormat: RootJsonFormat[Message] = jsonFormat2(Message)
-
-  final def routes: Route =
-    pathPrefix("harrypotter") {
-      pathEnd {
-        get {
-          println(Message(StatusCodes.OK.intValue, Seq("Harry Potter Series!")))
-          complete(
-            Message(StatusCodes.OK.intValue, Seq("Harry Potter Series!")))
-        }
-      } ~ path("helloworld") {
-        get {
-          println(
-            Message(StatusCodes.OK.intValue,
-                    Seq("Harry Potter Series!", "Hello World!")))
-          complete(
-            Message(StatusCodes.OK.intValue,
-                    Seq("Harry Potter Series!", "Hello World!")))
-        }
-      }
-    }
 
 }
