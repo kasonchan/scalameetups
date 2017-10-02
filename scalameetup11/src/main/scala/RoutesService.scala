065656e8214d48b102750de3c0b0904f2356cd54
kasonchan <kasonl.chan@gmail.com>
2017-10-03 01:05:00 +0800

    Update scalameetup11

--- scalameetup11/src/main/scala/RoutesService.scala
+++ scalameetup11/src/main/scala/RoutesService.scala
@@ -1,10 +1,7 @@
-import javax.ws.rs._
-
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import io.swagger.annotations.Api
 import spray.json.DefaultJsonProtocol.{jsonFormat2, _}
 import spray.json.RootJsonFormat
 
--- scalameetup11/src/main/scala/SwaggerService.scala
+++ scalameetup11/src/main/scala/SwaggerService.scala
@@ -7,11 +7,7 @@ import io.swagger.models.auth.BasicAuthDefinition
   * @since Sep-2017
   */
 object SwaggerService extends SwaggerHttpService {
-<<<<<<< HEAD
   override val apiClasses = Set(classOf[RoutesService])
-=======
-  override val apiClasses = Set(classOf[HarryPotterService])
->>>>>>> 1e0d535a7897921c6847aff88579d8bc4f66a82f
   override val host = "localhost:9000"
   override val info = Info(version = "1.0")
   override val securitySchemeDefinitions = Map(
